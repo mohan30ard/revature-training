c36c58fc085bb9cb0daccd4d53a817279723ada2
mohan30ard <mohantulabandhu@gmail.com>
2021-06-29 19:34:44 +0530

    operatorsprac

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/a1/b0a32ff7a5d8001b1a7afcc9a5e3480c
@@ -0,0 +1,12 @@
+package myfirstpack;
+
+public class Mypack {
+
+	public static void main(String[] args) {
+		
+		System.out.println("helloo.......its mme");
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ week-1 operators/src/operatorsprac/OperatorsMain.java
@@ -0,0 +1,13 @@
+package operatorsprac;
+
+public class OperatorsMain {
+
+	public static void main(String[] args) {
+		
+		Operators o = new Operators(5,6);
+		o.compare();
+		o.increment();
+		o.forinc();
+	}
+
+}
